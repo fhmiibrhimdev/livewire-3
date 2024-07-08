60ed42ed35048aec42983187080b47b3069fe0ec
fhmiibrhimdev <fahmidev.ibrahim@gmail.com>
2024-07-08 09:06:25 +0700

    add: laratrust, livewire, breeze, template admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Livewire\Example\Example;
+use App\Livewire\Profile\Profile;
+use App\Livewire\Dashboard\Dashboard;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Livewire\Control\User;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+                ->name('login');
+
+Route::post('/', [AuthenticatedSessionController::class, 'store']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/profile', Profile::class);
 });
+
+Route::group(['middleware' => ['auth', 'role:admin']], function() {
+    Route::get('/example', Example::class);
+    Route::get('/control-user', User::class);
+});
+
+Route::group(['middleware' => ['auth', 'role:user']], function() {
+
+});
+
+require __DIR__.'/auth.php';
